cff7f037bbb5d13fe319185e09c397809dbb4136
mrnicebrei <114418675+mrnicebrei@users.noreply.github.com>
2022-10-01 09:57:19 +0200

    removed print from multiplication function

--- Ex01.py
+++ Ex01.py
@@ -3,16 +3,14 @@ Written by: Thomas Beckmann
 Purpose: Multiplies 2 randomly generated 3x3 matrices and displays the result.
 """
 import numpy as np
-
-def MultMatrix(InA,InB): # multiplication function
-    MatC = InA@InB
-    print("This is your result:\n",MatC)
-    
+def MultMatrix(inA,inB): # multiplication function
+    out = inA@inB
+    return out
 print("This application multiplies two randomly generated three by three matrices and returns the result.")
 print("These are your randomly generated matrices: ")
 MatA = np.random.randint(0,10,(3,3))
 MatB = np.random.randint(0,10,(3,3))
 print("Matrix A:\n",MatA)
 print("Matrix B:\n",MatB)
-
-MultMatrix(MatA,MatB)
+MatC = MultMatrix(MatA,MatB)
+print("This is your result:\n",MatC)
