03f66f64621c50827cdb998c7fa2e48045cf14b9
mrnicebrei <114418675+mrnicebrei@users.noreply.github.com>
2022-12-19 23:05:05 +0100

    Update Ex02.py
    adjusted according to feedback

--- Ex02/Ex02.py
+++ Ex02/Ex02.py
@@ -9,7 +9,7 @@ Beckmann, Thomas
 November 2022
 """
 
-class particle:
+class Particle:
 	def __init__(self,massnumber,chargenumber): # instantiate class by giving Z and A
 		self.massnumber = massnumber
 		self.chargenumber = chargenumber
@@ -17,13 +17,13 @@ class particle:
 	def calc_neutrons (self): # method to calculate N
 		self.neutronnumber = self.massnumber - self.chargenumber
 
-	def identify(self):	#particle identifies itself, only prints N if calculated
+	def identify(self):	# particle identifies itself, only prints N if calculated
 		print(f"My Massnumber is {self.massnumber} and my Chargenumber is {self.chargenumber}.")
 		if hasattr(self,"neutronnumber"):
 			print (f"My Number of Neutrons is: {self.neutronnumber}")
 
 def main():
-	Carbon = particle(12,6)
+	Carbon = Particle(12,6)
 	Carbon.calc_neutrons()
 	Carbon.identify()
 
